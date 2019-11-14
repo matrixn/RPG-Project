6e245b42e5dcacf79d52e34981d9a3e022fdae35
Alex-ROG <raduta.alex@gmail.com>
2019-11-14 01:39:55 +0000

    Adding portals and spawn points

---
+++ Assets/Scripts/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PersistentObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] GameObject persistentObjectPrefab;
+
+        //remember static vars are not killed with class but with app!
+        static bool hasSpawned = false;
+
+        private void Awake()
+        {
+            if (hasSpawned) return;
+
+            SpawnPersistentObjects();
+
+            hasSpawned = true;
+        }
+
+        private void SpawnPersistentObjects()
+        {
+            GameObject persistentObject = Instantiate(persistentObjectPrefab);
+            DontDestroyOnLoad(persistentObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+namespace RPG.SceneManagement
+{
+
+    public class Portal : MonoBehaviour
+    {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E, F
+        }
+
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 2f;
+        [SerializeField] float fadeInTime = 2f;
+        [SerializeField] float fadeWaitTime = 0.7f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load is not set!");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            //star fade out
+            Fader fader = FindObjectOfType<Fader>();
+            yield return fader.FadeOut(fadeOutTime);
+
+            //load async next scene
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            //wait betwen fade out and fade in
+            yield return new WaitForSeconds(fadeWaitTime);
+
+            yield return fader.FadeIn(fadeInTime);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+    }
+}
